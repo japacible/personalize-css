ff9257b40dc9d4d9515870b3773d1283f92551f0
japacible <japacible@gmail.com>
2016-01-17 21:24:55 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,22 @@
+var siteToCSS = [
+  {site:"facebook.com", sheet:"facebook.css"},
+  {site:"messenger.com", sheet:"messenger.css"},
+  {site: "play.google.com/music", sheet:"playmusic.css"},
+];
+
+var modifyCSS = function() {
+  var address = window.location.href;
+
+  for (var i = 0; i < siteToCSS.length; i++) {
+    if (address.indexOf(siteToCSS[i].site) > 0) {
+      var link = document.createElement("link");
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.href = chrome.extension.getURL(siteToCSS[i].sheet);
+      document.getElementsByTagName("head")[0].appendChild(link);
+      return;
+    }
+  }
+}
+
+modifyCSS();
